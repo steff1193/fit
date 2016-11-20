e347afe54fab8e5dc84b8f0e6018aa2ab7ee40bd
Per Steffensen <steff@designware.dk>
2016-11-20 10:54:24 +0100

    Upgrading meteor-rxjs and getting rid of fontawsome (ionic icons is enough for now).

--- client/imports/mobileapp/app.module.ts
+++ client/imports/mobileapp/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from "./login/login.component";
   ],
   // Entry Components
   entryComponents: [
+    AppComponent,
     LoginComponent,
     ExerciseTemplatesComponent
   ],
